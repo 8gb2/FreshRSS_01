551bd293cfe5414f93496d2cfee572a5a4e45ce4
8gb2 <181823995+8gb2@users.noreply.github.com>
2024-09-19 03:07:33 +0800

    Fixed a path issue

--- cli/clear-favicon-cashe.sh
+++ cli/clear-favicon-cashe.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo Deleting all Favicons in the cashe
-rm /var/www/FreshRSS/cli/data/favicons/*.ico /var/www/FreshRSS/cli/data/favicons/*.txt
+rm /var/www/FreshRSS/data/favicons/*.ico /var/www/FreshRSS/data/favicons/*.txt
 echo Cashe clear
